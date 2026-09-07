7e9400c200be3cd4c9aedf195be6b3a96935ab99
HakkinDavid <desr11@hotmail.com>
2026-09-06 17:41:42 -0700

    Update mexico_emission_rules.dart

--- lib/src/features/catalog/domain/numismatics/data/mexico_emission_rules.dart
+++ lib/src/features/catalog/domain/numismatics/data/mexico_emission_rules.dart
@@ -590,7 +590,7 @@ const List<NumismaticEmissionRuleData> mexicoEmissionRules = [
       '20': 'Bimetálica',
       '50': 'Bimetálica',
     },
-    commemorativeDenominations: {'0.05', '0.10', '0.20', '0.50', '1', '2', '5', '10', '20', '50'},
+    commemorativeDenominations: {'1', '2', '5', '10', '20', '50'},
     commemorativeMotifsByDenomination: {
       '0.05': [
         NumismaticMotifRule('Nuevo Peso', 1992, 1995),
